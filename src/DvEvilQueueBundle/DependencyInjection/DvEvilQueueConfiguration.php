732b1813ea5722a67f7a3266599c6382547219a7
Антон Власов <druidvav@gmail.com>
2019-04-18 23:09:17 +0300

    fix

--- src/DvEvilQueueBundle/DependencyInjection/DvEvilQueueConfiguration.php
+++ src/DvEvilQueueBundle/DependencyInjection/DvEvilQueueConfiguration.php
@@ -9,7 +9,7 @@ class DvEvilQueueConfiguration implements ConfigurationInterface
     /**
      * Generates the configuration tree builder.
      *
-     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder
+     * @return TreeBuilder The tree builder
      */
     public function getConfigTreeBuilder()
     {
@@ -23,6 +23,7 @@ class DvEvilQueueConfiguration implements ConfigurationInterface
                 scalarNode("workers")->defaultValue(1)->end()->
                 scalarNode("priority_workers")->defaultValue(1)->end()->
                 scalarNode("debug")->defaultFalse()->end()->
+                scalarNode("restart_after")->defaultValue(500)->end()->
                 scalarNode("usleep_after_request")->defaultValue(50000)->end()->
                 scalarNode("usleep_after_empty")->defaultValue(2000000)->end()->
             end()

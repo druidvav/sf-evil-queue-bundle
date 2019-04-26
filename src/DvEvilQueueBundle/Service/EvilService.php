64c36ae5b489bb2bc8aae866c5a36e49bbce6ef4
Антон Власов <druidvav@gmail.com>
2019-04-26 11:53:56 +0300

    fix

--- src/DvEvilQueueBundle/Service/EvilService.php
+++ src/DvEvilQueueBundle/Service/EvilService.php
@@ -44,7 +44,7 @@ class EvilService
             'protocol' => $request->getProtocol(),
             'url' => $request->getUrl(),
             'method' => $request->getMethod(),
-            'request_param' => json_encode($request->getRequestParam()),
+            'request_param' => $request->isJsonRpc() ? json_encode($request->getRequestParam()) : serialize($request->getRequestParam()),
             'create_date' => date('Y-m-d H:i:s'),
             'request_timeout' => $request->getRequestTimeout(),
         ]);
